186be8d84edb361c34097c66114c85377975b199
최준성 <147041315+yumXD@users.noreply.github.com>
2024-08-19 16:24:25 +0900

    Feat: 이력서 페이지 제목, 설명 추가

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -4,6 +4,12 @@ import Education from "@/app/components/Education";
 import TechStack from "@/app/components/TechStack";
 import AboutMe from "@/app/components/AboutMe";
 import WithBasicInfoLayout from "@/app/layouts/WithBasicInfoLayout";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "이력서",
+    description: "이력서 사이트입니다."
+};
 
 export default function Page() {
     return (
