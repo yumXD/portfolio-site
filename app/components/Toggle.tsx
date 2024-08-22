d3f789d667827f1b4ca2d1c0d77cc201443d0993
최준성 <147041315+yumXD@users.noreply.github.com>
2024-08-22 18:26:41 +0900

    Feat: 토글 컴포넌트 스타일 변경

--- app/components/Toggle.tsx
+++ app/components/Toggle.tsx
@@ -1,27 +1,30 @@
 "use client";
 
-import {Box, FormControl, FormLabel, Switch} from "@chakra-ui/react";
+import {Box, Button, ButtonGroup} from "@chakra-ui/react";
 import {usePathname, useRouter} from "next/navigation";
 import {useEffect, useState} from "react";
 
 export default function Toggle() {
     const pathname = usePathname();
     const router = useRouter();
-    const [isChecked, setIsChecked] = useState(pathname === "/portfolio");
-
-    const handleToggle = () => {
-        const newCheckedState = !isChecked;
-        setIsChecked(newCheckedState);
-        if (newCheckedState) {
-            router.push("/portfolio");
-        } else {
-            router.push("/resume");
-        }
-    };
+    const [selectedPage, setSelectedPage] = useState("");
 
     useEffect(() => {
-        setIsChecked(pathname === "/portfolio");
+        if (pathname === "/portfolio") {
+            setSelectedPage("portfolio");
+        } else if (pathname === "/resume") {
+            setSelectedPage("resume");
+        } else if (pathname === "/introduction") {
+            setSelectedPage("introduction");
+        }
     }, [pathname]);
+
+    const handleNavigation = (page: string) => {
+        if (page !== selectedPage) {
+            setSelectedPage(page);
+            router.push(`/${page}`);
+        }
+    };
 
     return (
         <Box mt={6}
@@ -33,12 +36,32 @@ export default function Toggle() {
              width="100%"
              mx="auto"
              minWidth="300px">
-            <FormControl display="flex" alignItems="center">
-                <FormLabel htmlFor="toggle-switch" mb="0">
-                    {isChecked ? "Portfolio" : "Resume"}
-                </FormLabel>
-                <Switch id="toggle-switch" isChecked={isChecked} onChange={handleToggle}/>
-            </FormControl>
+            <ButtonGroup isAttached>
+                <Button
+                    colorScheme={selectedPage === "resume" ? "blue" : "gray"}
+                    onClick={() => handleNavigation("resume")}
+                    isDisabled={selectedPage === "resume"}
+                    _disabled={{cursor: "default"}}
+                >
+                    이력서
+                </Button>
+                <Button
+                    colorScheme={selectedPage === "portfolio" ? "blue" : "gray"}
+                    onClick={() => handleNavigation("portfolio")}
+                    isDisabled={selectedPage === "portfolio"}
+                    _disabled={{cursor: "default"}}
+                >
+                    포트폴리오
+                </Button>
+                <Button
+                    colorScheme={selectedPage === "introduction" ? "blue" : "gray"}
+                    onClick={() => handleNavigation("introduction")}
+                    isDisabled={selectedPage === "introduction"}
+                    _disabled={{cursor: "default"}}
+                >
+                    자기소개서
+                </Button>
+            </ButtonGroup>
         </Box>
     );
 }
--- app/layouts/WithBasicInfoLayout.tsx
+++ app/layouts/WithBasicInfoLayout.tsx
@@ -1,5 +1,6 @@
 import {ReactNode} from "react";
 import {Box} from "@chakra-ui/react";
+import Toggle from "@/app/components/Toggle";
 import UserPage from "@/app/user/page";
 
 interface WithBasicInfoLayoutProps {
@@ -10,6 +11,7 @@ export default function WithBasicInfoLayout({children}: WithBasicInfoLayoutProps
     return (
         <Box p={8}>
             <UserPage/>
+            <Toggle/>
             {children}
         </Box>
     );
